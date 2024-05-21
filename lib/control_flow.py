4c209436c67dd36fc6d3c8054decc460cde0e499
molly <mollynzembi@gmail.com>
2024-05-21 23:37:09 +0300

    yet tofinish

--- lib/control_flow.py
+++ lib/control_flow.py
@@ -3,10 +3,28 @@
 def admin_login(username, password):
     # your code here
     pass
+# control_flow.py
+
+def admin_login(username, password):
+    if (username == "admin" or username == "ADMIN") and password == "12345":
+        return "Access granted"
+    else:
+        return "Access denied"
 
 def hows_the_weather(temperature):
     # your code here
     pass
+def hows_the_weather(temperature):
+    if temperature < 40:
+        response = "brisk"
+    elif temperature >= 40 and temperature <= 65:
+        response = "a little chilly"
+    elif temperature > 85:
+        response = "too dang hot"
+    else:
+        response = "perfect"
+    return f"It's {response} out there!"
+
 
 def fizzbuzz(num):
     # your code here
